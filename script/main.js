2f5e2d50d4bf66c4d9edf7e4fc748357be4744fe
Marco Parisi <marco.parisi@edu.itspiemonte.it>
2022-05-24 14:10:29 +0200

    Header
    header + start homepage

---
+++ script/main.js
@@ -0,0 +1,29 @@
+/* GENERAL CHECK */
+let menuToggle = document.querySelector("#menu-toggle");
+/*HOME */
+let home = document.querySelector("#menu-home-toggle");
+let menuHome = document.querySelector("#menu-home");
+/* NEWS */
+let news = document.querySelector("#menu-news-toggle");
+let menuNews = document.querySelector("#menu-news");
+/* OPEN FIRST BAR + closes everything*/
+menuToggle.onclick = function () {
+  menuToggle.classList.toggle("open");
+  if (menuToggle.className === "open") {
+    menuHome.classList.remove("active");
+    menuNews.classList.remove("active");
+  }
+}
+/*OPEN HOME SECONDBAR */
+home.onclick = function () {
+  if (menuToggle.className === "open") {
+    menuHome.classList.add("active");
+  }
+};
+/*OPEN NEWS SECONDBAR */
+news.onclick = function () {
+  if (menuToggle.className === "open") {
+    menuNews.classList.add("active");
+  }
+};
+/* va fatta la stessa cosa con i successivi href */
