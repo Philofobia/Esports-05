3d48247126f4bb1462a68f8ef0e1751926503aa4
MarcoParisi <marco.parisi@edu.itspiemonte.it>
2022-05-29 18:14:25 +0200

    Homepage done
    Slider gallery, more content.

--- script/main.js
+++ script/main.js
@@ -7,7 +7,7 @@ let menuHome = document.querySelector("#menu-home");
 let news = document.querySelector("#menu-news-toggle");
 let menuNews = document.querySelector("#menu-news");
 /*ANYTHING IN THE SUB-MENU */
-let subMenuItems = document.querySelectorAll(".menu-beta-item")
+let subMenuItems = document.querySelectorAll(".menu-beta-item");
 /* OPEN FIRST BAR + closes everything*/
 menuToggle.onclick = function () {
   if (menuToggle.className === "open") {
@@ -15,7 +15,7 @@ menuToggle.onclick = function () {
     menuNews.classList.remove("active");
   }
   menuToggle.classList.toggle("open");
-}
+};
 /*OPEN HOME SECONDBAR */
 home.onclick = function () {
   if (menuToggle.className === "open") {
@@ -29,8 +29,46 @@ news.onclick = function () {
   }
 };
 /* close mobile bar if link clicked */
-subMenuItems.forEach(element => element.onclick = function(){
-  menuToggle.classList.remove("open");
-  menuNews.classList.remove("active");
-})
+subMenuItems.forEach(
+  (element) =>
+    (element.onclick = function () {
+      menuToggle.classList.remove("open");
+      menuNews.classList.remove("active");
+      menuHome.classList.remove("active");
+    })
+);
 /* va fatta la stessa cosa con i successivi href */
+
+/* MOBILE SLIDER */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("img-square");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
